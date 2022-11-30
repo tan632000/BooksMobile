7c530439bfbb7cf8cea183d41ae0b85f518f36d3
Hoàng Văn Tân <tan.hoang@skyblazestudios.com>
2022-11-30 21:20:01 +0700

    Add config apolo

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const API_KEY = "marana::stepzen.net+1000::9415a2b417a41c17f8d8294adcbc6a2ed08efbcbc4a9c4ea38f645b7d38acfa8";
+
+const client = new ApolloClient({
+  uri: "https://marana.stepzen.net/api/punk-peacock/__graphql",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,7 +25,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+        </ApolloProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
